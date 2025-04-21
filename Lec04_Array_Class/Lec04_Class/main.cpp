3b48db75da786d2d568f74ce6272e113cee9f8d0
sonxunghyn <thargum8027@naver.com>
2025-04-21 12:57:09 +0900

    Lec04

---
+++ Lec04_Array_Class/Lec04_Class/main.cpp
@@ -0,0 +1,25 @@
+#include "Complex.h"
+#include "SportsCar.h"
+
+void main()
+{
+	//Complex a, b, c;
+	//a.read( "A =");
+	//b.read( "B =");
+	//c.add(a, b);
+	//a.print ("A = ");
+	//b.print ("B = ");
+	//c.print ("A+B =");
+
+	Car myCar;
+	Car momsCar(10, "K5", 2);
+
+	myCar.whereAmI();
+	momsCar.whereAmI();
+	myCar.changeGear(3);
+	momsCar.speedUp();
+	momsCar.display();
+
+}
+
+
